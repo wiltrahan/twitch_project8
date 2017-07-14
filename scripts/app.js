a62830094b6d3c1c7f92c138cac0b5590fc80f33
wiltrahan <twilorip@gmail.com>
2017-07-14 17:51:26 -0400

    first call display on screen

--- scripts/app.js
+++ scripts/app.js
@@ -5,7 +5,39 @@ var model = {
   twitchList: []
 };
 
-var users = [
+// var users = [
+//   "ESL_SC2",
+//   "OgamingSC2",
+//   "cretetion",
+//   "freecodecamp",
+//   "storbeck",
+//   "habathcx",
+//   "RobotCaleb",
+//   "noobs2ninjas"
+// ];
+
+//pass users array, loop through each name
+//push into twitchListarray
+
+// function twitchLookup(users) {
+//   for (var i = 0; i < users.length; i++) {
+//     $.ajax({
+//       url: route + "channels/" + users[i] + "?client_id=" + myKey,
+//       success: function(response) {
+//         model.twitchList.push(response);
+//         // console.log(response);
+//         // model.twitchList = response.results;
+
+//       }
+//     });
+//   }
+//   // displayUsers();
+// console.log(model.twitchList);
+
+// };
+//
+function twitchLookup() {
+  var users = [
   "ESL_SC2",
   "OgamingSC2",
   "cretetion",
@@ -15,20 +47,49 @@ var users = [
   "RobotCaleb",
   "noobs2ninjas"
 ];
+  $.each(users, function(i) {
+    $.ajax({
+      url: route + "channels/" + users[i] + "?",
+      data: {
+        client_id: myKey
+      },
+      success: function(response) {
+        model.twitchList.push(response);
 
-function twitchLookup(user) {
-  $.ajax({
-    url: route + user + "?client_id=" + myKey,
-    success: function(response) {
-      model.twitchList.push(response);
-      console.log(response);
-      console.log(model.twitchList);
-    }
+      }
+    })
+
+  });
+
+  // console.log(model.twitchList);
+  // displayUsers();
+  setTimeout(function(){ displayUsers(); }, 200);
+};
+
+
+// var view = {
+//   displayUsers: function() {
+//     model.twitchList.forEach(function(user) {
+//       var name = $("<h4></h4>").text(user.name)
+//       var nameView = $("<li></li>")
+//         .attr("class", "name-group-item")
+//         .append(name);
+//         $("#users ul").append(nameView);
+//     });
+//   }
+// };
+
+function displayUsers() {
+  model.twitchList.forEach(function(user) {
+    var name = $("<h4></h4>").text(user.name);
+    var nameView = $("<li></li>").attr("class", "name-group-item").append(name);
+    $("#users ul").append(nameView);
   });
 }
 
-// console.log(twitchLookup(users[3]));
-
 $(document).ready(function() {
-  twitchLookup(users[3]);
+  twitchLookup();
+  // displayUsers(twitchLookup(users))
 });
+
+// twitchLookup();
