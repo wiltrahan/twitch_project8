e6ae8562eb7844d8790abb65bfaed366b356e462
wiltrahan <twilorip@gmail.com>
2017-07-19 18:28:01 -0400

    add online call

--- scripts/app.js
+++ scripts/app.js
@@ -2,52 +2,76 @@ var myKey = config.token;
 var route = config.route;
 
 var model = {
-  twitchList: []
+  twitchList: [],
+  twitchOffline: []
 };
-
-function twitchLookup() {
-  var users = [
+var users = [
   "ESL_SC2",
   "OgamingSC2",
   "cretetion",
   "freecodecamp",
-  // "storbeck",
+  "storbeck",
   "habathcx",
   "RobotCaleb",
   "noobs2ninjas"
 ];
+
+function twitchLookup() {
   $.each(users, function(i) {
     $.ajax({
-      url: route + "channels/" + users[i] + "?",
+      url: route + users[i] + "?",
       data: {
         client_id: myKey
       },
       success: function(response) {
-        model.twitchList.push(response);
-
+        if(response.stream !== null) {
+          model.twitchList.push(response);
+        } else {
+          $.ajax({
+            url: "https://api.twitch.tv/kraken/channels/" + users[i] + "?",
+            data: {
+              client_id: myKey
+            },
+            success: function(response){
+              model.twitchOffline.push(response);
+            }
+          })
+        }
       }
     })
-
   });
-  setTimeout(function(){ displayUsers(); }, 600);
+  setTimeout(function(){ displayUsers(); }, 1000);
 };
+//model.twitchOffline.push(response);
 
-
+// model.twitchList[0].stream.channel.name
 
 
 function displayUsers() {
-  model.twitchList.forEach(function(user) {
-    var name = $("<h4></h4>").text(user.display_name);
-    var status = $("<p></p>").text(user.status);
-    var logo = $("<img>").attr("src", user.logo);
-    var link = $("<a>").attr("href", user.url).append(name);
+  var name, status, logo, link, nameView,
+  offName, offStatus, offLogo, offLink, offNameView;
 
-    // $('<a>').append(name);
+    model.twitchList.forEach(function(user) {
+      name = $("<h4></h4>").text(user.stream.channel.name);
+      status = $("<p></p>").text(user.stream.channel.status);
+      logo = $("<img>").attr("src", user.stream.channel.logo);
+      link = $("<a>").attr("href", user.stream.channel.url).append(name);
+      nameView = $("<li>").attr("class", "name-group-item").append(link, logo, status).addClass('online');
+      $("#users ul").append(nameView);
+    });
 
-    var nameView = $("<li>").attr("class", "name-group-item").append(link, logo, status);
+    model.twitchOffline.forEach(function(offUser) {
+    offName = $("<h4></h4>").text(offUser.display_name);
+    offStatus = $("<p></p>").text(offUser.status);
+    offLogo = $("<img>").attr("src", offUser.logo);
+    offLink = $("<a>").attr("href", offUser.url).append(offName);
 
-    $("#users ul").append(nameView);
+
+    offNameView = $("<li>").attr("class", "name-group-item").append(offLink, offLogo, offStatus);
+
+    $("#users ul").append(offNameView);
   });
+
 }
 
 $(document).ready(function() {
@@ -55,7 +79,9 @@ $(document).ready(function() {
 });
 
 
+//first call to stream
+//if not null, get name, logo, game, stream_type=live or not, status
+//if null call to https://api.twitch.tv/kraken?
+//get name, logo
 
-
-
-$
+// url: route + "channels/" + users[i] + "?",
