bd87c4d774007985590181ab7c7f55aff0a0bdfb
wiltrahan <twilorip@gmail.com>
2017-07-20 19:15:21 -0400

    added 404 placeholder

--- scripts/app.js
+++ scripts/app.js
@@ -3,7 +3,8 @@ var route = config.route;
 
 var model = {
   twitchList: [],
-  twitchOffline: []
+  twitchOffline: [],
+  twitchNotFound: [],
 };
 var users = [
   "ESL_SC2",
@@ -13,7 +14,8 @@ var users = [
   // "storbeck",
   "habathcx",
   "RobotCaleb",
-  "noobs2ninjas"
+  "noobs2ninjas",
+  "brunofin"
 ];
 
 //2. gets users from array
@@ -29,7 +31,7 @@ function twitchLookup() {
         client_id: myKey
       },
       success: function(response) {
-        if(response.stream !== null) {
+        if (response.stream !== null) {
           model.twitchList.push(response);
         } else {
           $.ajax({
@@ -37,43 +39,77 @@ function twitchLookup() {
             data: {
               client_id: myKey
             },
-            success: function(response){
+            success: function(response) {
               model.twitchOffline.push(response);
+            },
+            error: function() {
+              model.twitchNotFound.push(response);
             }
-          })
+          });
         }
       }
-    })
+    });
   });
-  setTimeout(function(){ displayUsers(); }, 1000);
-};
+  setTimeout(function() {
+    displayUsers();
+  }, 1000);
+}
 
 //3. iterate over each array and show on screen
 function displayUsers() {
-  var name, status, logo, link, nameView, viewers,
-  offName, offStatus, offLogo, offLink, offNameView, isOnline;
+  var name,
+    status,
+    logo,
+    link,
+    nameView,
+    viewers,
+    offName,
+    offStatus,
+    offLogo,
+    offLink,
+    offNameView,
+    isOnline,
+    noLink,
+    noAccount,
+    noNameView;
 
-    model.twitchList.forEach(function(user) {
-      isOnline = $("<h3></h3>").text("Streaming Live Now!").addClass('isLive');
-      name = $("<h4></h4>").text(user.stream.channel.display_name);
-      status = $("<p></p>").text(user.stream.channel.status);
-      logo = $("<img>").attr("src", user.stream.channel.logo);
-      viewers = $("<p></p>").text(user.stream.viewers + ' currently viewing').addClass('viewers');
-      link = $("<a>").attr("href", user.stream.channel.url).append(name);
-      nameView = $("<li>").attr("class", "name-group-item").append(isOnline, link, logo, status, viewers).addClass('online');
-      $("#users ul").append(nameView);
-    });
+  model.twitchList.forEach(function(user) {
+    isOnline = $("<h3></h3>").text("Streaming Live Now!").addClass("isLive");
+    name = $("<h4></h4>").text(user.stream.channel.display_name);
+    status = $("<p></p>").text(user.stream.channel.status);
+    logo = $("<img>").attr("src", user.stream.channel.logo);
+    viewers = $("<p></p>")
+      .text(user.stream.viewers + " currently viewing")
+      .addClass("viewers");
+    link = $("<a>").attr("href", user.stream.channel.url).append(name);
+    nameView = $("<li>")
+      .attr("class", "name-group-item")
+      .append(isOnline, link, logo, status, viewers)
+      .addClass("online");
+    $("#users ul").append(nameView);
+  });
 
-    model.twitchOffline.forEach(function(offUser) {
-      offName = $("<h4></h4>").text(offUser.display_name);
-      offStatus = $("<p></p>").text('currently offline');
-      offLogo = $("<img>").attr("src", offUser.logo);
-      offLink = $("<a>").attr("href", offUser.url).append(offName);
-      offNameView = $("<li>").attr("class", "name-group-item").append(offLink, offLogo, offStatus);
-      $("#users ul").append(offNameView);
-    });
-};
+  model.twitchOffline.forEach(function(offUser) {
+    offName = $("<h4></h4>").text(offUser.display_name);
+    offStatus = $("<p></p>").text("currently offline");
+    offLogo = $("<img>").attr("src", offUser.logo);
+    offLink = $("<a>").attr("href", offUser.url).append(offName);
+    offNameView = $("<li>")
+      .attr("class", "name-group-item")
+      .append(offLink, offLogo, offStatus);
+    $("#users ul").append(offNameView);
+  });
 
+  model.twitchNotFound.forEach(function(notFound) {
+    noLink = $("<p></p>").text(notFound._links.channel);
+    noAccount = $("<p></p>").html("STATUS 404" + '<br>' + "aka: This fool deleted his/her account!");
+    noNameView = $("<li>")
+      .attr("class", "name-group-item")
+      .append(noAccount, noLink);
+    $("#users ul").append(noNameView);
+
+  });
+}
 
 //1. on ready call twitchLookup
 $(document).ready(function() {
@@ -81,3 +117,4 @@ $(document).ready(function() {
 });
 
 
+// model.twitchNotFound[0]._links.channel
